6dd044ad20d42f783cd139a865f06aa6015d3096
Austin Prete <acp2142@gmail.com>
2011-06-19 19:56:06 -0600

    update

--- haskell/project_euler/euler48.hs
+++
@@ -1,2 +0,0 @@
--- Problem 48 Find the last ten digits of 11 + 22 + ... + 10001000.
-main = print (sum [x ^ x | x <- [1..1000]])
--- haskell/project_euler/euler9.hs
+++
@@ -1,2 +0,0 @@
--- Problem 9 Find the only Pythagorean triplet, {a, b, c}, for which a + b + c = 1000.
-main = print [(a*b*c) | c <- [1..1000], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2, a + b + c == 1000]
