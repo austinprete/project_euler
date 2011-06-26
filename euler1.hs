be1b180e38a9dc014487474db8853dfd6ab82970
Austin Prete <acp2142@gmail.com>
2011-06-26 01:08:43 -0600

    changed euler1

--- euler1.hs
+++ euler1.hs
@@ -1,2 +1,4 @@
 -- Problem 1 Add all the natural numbers below one thousand that are multiples of 3 or 5.
-main = print ((sum [3,6..999]) + (sum [x | x <- [5,10..999], x `rem` 3 /= 0]))
+main = print euler1
+
+euler1 = (sum [3,6..999]) + (sum [x | x <- [5,10..999], x `rem` 3 /= 0])
